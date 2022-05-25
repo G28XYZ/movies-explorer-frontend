9bc6c1b0ec014d51acd00c00f1775001c5dd180c
Александр Кучеренко <g28xyz@ya.ru>
2022-05-25 15:07:11 +0700

    feat: сверстать header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,20 @@
+import { Link } from "react-router-dom";
+import logo from "../../images/logo.svg";
+
 function Header() {
-  return <>Header</>;
+  return (
+    <header className="header">
+      <img src={logo} alt="Логотип" />
+      <nav className="header__links">
+        <Link to="/sign-up" className="header__link">
+          Регистрация
+        </Link>
+        <Link to="/sign-in" className="header__link color_primary">
+          Войти
+        </Link>
+      </nav>
+    </header>
+  );
 }
 
 export default Header;
