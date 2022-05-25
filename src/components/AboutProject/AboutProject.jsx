d43863fd99948c2bf503ce0bcaeb43150f418a13
Aleksandr Kucherenko <g28xyz@ya.ru>
2022-05-25 21:33:51 +0700

    feat: досверстать AboutProject

--- src/components/AboutProject/AboutProject.jsx
+++ src/components/AboutProject/AboutProject.jsx
@@ -2,24 +2,30 @@ function AboutProject() {
   return (
     <section className="project">
       <h2 className="project__title text_title">О проекте</h2>
-      <div className="project__about">
-        <div>
-          <h3 className="project__about-title text_title">
-            Дипломный проект включал 5 этапов
-          </h3>
+      <ul className="project__about">
+        <li>
+          <h3 className="project__about-title text_title">Дипломный проект включал 5 этапов</h3>
           <p className="project__about-text text">
-            Составление плана, работу над бэкендом, вёрстку, добавление
-            функциональности и финальные доработки.
+            Составление плана, работу над бэкендом, вёрстку, добавление функциональности и финальные
+            доработки.
           </p>
+        </li>
+        <li>
+          <h3 className="project__about-title text_title">На выполнение диплома ушло 5 недель</h3>
+          <p className="project__about-text text">
+            У каждого этапа был мягкий и жёсткий дедлайн, которые нужно было соблюдать, чтобы
+            успешно защититься.
+          </p>
+        </li>
+      </ul>
+      <div className="project__duration">
+        <div className="project__duration-area">
+          <div className="project__duration-time text color_primary">1 неделя</div>
+          <p className="project__duration-name text color_text">Back-end</p>
         </div>
-        <div>
-          <h3 className="project__about-title text_title">
-            На выполнение диплома ушло 5 недель
-          </h3>
-          <p className="project__about-text text">
-            У каждого этапа был мягкий и жёсткий дедлайн, которые нужно было
-            соблюдать, чтобы успешно защититься.
-          </p>
+        <div className="project__duration-area">
+          <div className="project__duration-time text color_secondary">4 недели</div>
+          <p className="project__duration-name text color_text">Front-end</p>
         </div>
       </div>
     </section>
--- src/components/Promo/Promo.jsx
+++ src/components/Promo/Promo.jsx
@@ -1,14 +1,12 @@
 function Promo() {
   return (
     <section className="promo color_background">
-      <h1 className="promo__title">
-        Учебный проект студента факультета Веб-разработки.
-      </h1>
+      <h1 className="promo__title">Учебный проект студента факультета Веб-разработки.</h1>
       <nav>
         <ul className="promo__list">
-          <li className="promo__list-item color_button text">О проекте</li>
-          <li className="promo__list-item color_button text">Технологии</li>
-          <li className="promo__list-item color_button text">Студент</li>
+          <li className="promo__list-item color_secondary text">О проекте</li>
+          <li className="promo__list-item color_secondary text">Технологии</li>
+          <li className="promo__list-item color_secondary text">Студент</li>
         </ul>
       </nav>
     </section>
