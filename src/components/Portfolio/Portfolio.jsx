61ed91d5941e18e712fbd66617c36c4cc2cb632b
Александр Кучеренко <g28xyz@ya.ru>
2022-05-26 10:10:49 +0700

    feat: досверстать Portfolio

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -1,21 +1,26 @@
+import arrow from "../../images/arrow.svg";
+
 function Portfolio() {
   return (
     <section className="portfolio">
       <p className="portfolio__title text color_text">Портфолио</p>
       <ul className="portfolio__list">
-        <li className="portfolio__list-item">
-          <a className="portfolio__link text_medium underline-p20" href="/#">
-            Статичный сайт
+        <li className="portfolio__list-item underline-p20">
+          <a className="portfolio__link" href="/#">
+            <p className="text_medium">Статичный сайт</p>
+            <img src={arrow} alt="Иконка - ссылочная стрека" />
+          </a>
+        </li>
+        <li className="portfolio__list-item underline-p20">
+          <a className="portfolio__link " href="/#">
+            <p className="text_medium">Адаптивный сайт</p>
+            <img src={arrow} alt="Иконка - ссылочная стрека" />
           </a>
         </li>
         <li className="portfolio__list-item">
-          <a className="portfolio__link text_medium underline-p20" href="/#">
-            Адаптивный сайт
-          </a>
-        </li>
-        <li className="portfolio__list-item">
-          <a className="portfolio__link text_medium" href="/#">
-            Одностраничное приложение
+          <a className="portfolio__link" href="/#">
+            <p className="text_medium">Одностраничное приложение</p>
+            <img src={arrow} alt="Иконка - ссылочная стрека" />
           </a>
         </li>
       </ul>
