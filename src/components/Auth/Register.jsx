6644e5a5f84ea4a9488ccb58a18afbcc4b8ff1a3
Aleksandr Kucherenko <g28xyz@ya.ru>
2022-05-29 21:10:20 +0700

    feat: добвить author in package

--- src/components/Auth/Register.jsx
+++ src/components/Auth/Register.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { useNavigate, Link } from "react-router-dom";
 import logo from "../../images/logo.svg";
 import Input from "./Input";
